fd8237394612daf8e6053120ff1e6a4e549747c7
Prof Monaliza <83433815+profmonaliza@users.noreply.github.com>
2022-11-10 17:27:54 -0300

    Update jsDoAluno.js

--- jsDoAluno.js
+++ jsDoAluno.js
@@ -1,27 +1,16 @@
-canvas = document.getElementById('???'); // guardando o canva dentro de uma variável para que eu possa manipular ele depois
-ctx = canvas.getContext("2d"); // tipo do meu canva
 
-roverWidth = ??; // definir a largura
-roverHeight = ??; // definir a altura
-
-backgroundImage = "mars.jpg";
-
-roverImage = "rover.png";
-
-roverX = 10; // posição no eixo x
-roverY = 10; // posição no eixo y 
 
 function add() {
-	backgroundImgTag = new Image(); //definindo uma variável com uma nova imagem
+	backgroundImgTag =???(); //definindo uma variável com uma nova imagem
 	backgroundImgTag.onload = CarregarPlanoDeFundo; // ajustando uma função, ao carregar essa variável
 	backgroundImgTag.src = backgroundImage;   // carregar a imagem
 
-	roverImgTag = new Image(); //definindo uma variável com uma nova imagem
+	roverImgTag = ???(); //definindo uma variável com uma nova imagem
 	roverImgTag.onload = CarregarRover; // ajustando uma função, ao carregar essa variável
 	roverImgTag.src = roverImage;   // carregar a imagem
 }
 
-function CarregarPlanoDeFundo() {
+function ???() {
 	ctx.drawImage(backgroundImgTag, 0, 0, canvas.width, canvas.height);
 	// ctx= contexto
 	// drawimage = desenhar imagem
@@ -29,7 +18,7 @@ function CarregarPlanoDeFundo() {
 	// Qual imagem?? backgroundImgTag
 }
 
-function CarregarRover() {
+function ???() {
 	ctx.drawImage(roverImgTag, roverX, roverY, roverWidth, roverHeight);
 }
 
@@ -44,22 +33,22 @@ function myKeyDown(e)
 	
 	console.log(keyPressed); // mostrará no console a tecla pressionada
 
-		if(keyPressed == '38') // se o valor da tecla pressionada for 38, irá acontecer a função 'cima()'
+		if(keyPressed == '??') // se o valor da tecla pressionada for 38, irá acontecer a função 'cima()'
 		{
 			cima();
 			
 		}
-		if(keyPressed == '40')
+		if(keyPressed == '??')
 		{
 			baixo();
 			
 		}
-		if(keyPressed == '37')
+		if(keyPressed == '??')
 		{
 			esquerda();
 			
 		}
-		if(keyPressed == '39')
+		if(keyPressed == '???')
 		{
 			direita();
 			
@@ -69,7 +58,7 @@ function cima()
 {
 	if(roverY >=0)
 	{
-		roverY = roverY - 10;
+		roverY = roverY ? 10;
 		CarregarPlanoDeFundo();
 		 CarregarRover();
 	}
@@ -78,7 +67,7 @@ function baixo()
 {
 	if(roverY <=500)
 	{
-		roverY =roverY+ 10;
+		roverY =roverY ? 10;
 		CarregarPlanoDeFundo();
 		CarregarRover();
 	}
@@ -87,7 +76,7 @@ function esquerda()
 {
 	if(roverX >= 0)
 	{
-		roverX =roverX - 10;
+		roverX =roverX ? 10;
 		CarregarPlanoDeFundo();
 		CarregarRover();
 	}
@@ -96,7 +85,7 @@ function direita()
 {
 	if(roverX <= 700)
 	{
-		roverX =roverX + 10;
+		roverX =roverX ? 10;
 		CarregarPlanoDeFundo();
 		CarregarRover();
    }
